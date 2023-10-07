0e150b48bdccbd83233ab26f3c4a8642a5c1e147
MmMaIIi <2360655509@qq.com>
2023-10-08 02:43:04 +0800

    first

--- user/echo.c
+++ user/echo.c
@@ -2,13 +2,14 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
+int 
 main(int argc, char *argv[])
 {
   int i;
 
   for(i = 1; i < argc; i++){
-    write(1, argv[i], strlen(argv[i]));
+    write(1, argv[i], strlen(argv[i])); // write(int fd, const void *buf, size_t count)
+    // write(-1, "aaaa", 4); 0 1 2 all successed
     if(i + 1 < argc){
       write(1, " ", 1);
     } else {
